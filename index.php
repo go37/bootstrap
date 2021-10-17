cff43a05ca3c0730b4a84e521ec50b04e4c812ce
go37 <90458094+go37@users.noreply.github.com>
2021-10-17 19:52:59 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,123 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bootstarp - Grid</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+</head>
+
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col-1 bg-dark text-dark">1</div>
+            <div class="col-9 bg-white text-white">2-10</div>
+            <div class="col-1 bg-dark text-dark">11</div>
+        </div>
+        <div class="row">
+            <div class="col-1 bg-dark text-dark border">1</div>
+            <div class="col-3 bg-white text-white">2-4</div>
+            <div class="col-1 bg-danger text-danger border">5</div>
+            <div class="col-1 bg-warning text-warning border">6</div>
+            <div class="col-1 bg-danger text-danger border">7</div>
+            <div class="col-3 bg-white text-white">8-10</div>
+            <div class="col-1 bg-dark text-dark border">11</div>
+        </div>
+        <div class="row">
+            <div class="col-1 bg-dark text-dark border">1</div>
+            <div class="col-2 bg-white text-white">2-3</div>
+            <div class="col-1 bg-danger text-danger border">4</div>
+            <div class="col-1 bg-warning text-warning border">5</div>
+            <div class="col-1 bg-dark text-dark border">6</div>
+            <div class="col-1 bg-warning text-warning border">7</div>
+            <div class="col-1 bg-danger text-danger border">8</div>
+            <div class="col-2 bg-white text-white">9-10</div>
+            <div class="col-1 bg-dark text-dark border">11</div>
+        </div>
+        <div class="row">
+            <div class="col-1 bg-dark text-dark border">1</div>
+            <div class="col-1 bg-dark text-dark border">2</div>
+            <div class="col-1 bg-danger text-danger border">3</div>
+            <div class="col-1 bg-danger text-danger border">4</div>
+            <div class="col-1 bg-warning text-warning border">5</div>
+            <div class="col-1 bg-dark text-dark border">6</div>
+            <div class="col-1 bg-warning text-warning border">7</div>
+            <div class="col-1 bg-danger text-danger border">8</div>
+            <div class="col-1 bg-danger text-danger border">9</div>
+            <div class="col-1 bg-dark text-dark border">10</div>
+            <div class="col-1 bg-dark text-dark border">11</div>
+        </div>
+        <div class="row">
+            <div class="col-1 bg-dark text-dark border">1</div>
+            <div class="col-1 bg-dark text-dark border">2</div>
+            <div class="col-1 bg-white text-white">3</div>
+            <div class="col-1 bg-danger text-danger border">4</div>
+            <div class="col-1 bg-warning text-warning border">5</div>
+            <div class="col-1 bg-dark text-dark border">6</div>
+            <div class="col-1 bg-warning text-warning border">7</div>
+            <div class="col-1 bg-danger text-danger border">8</div>
+            <div class="col-1 bg-white text-white">9</div>
+            <div class="col-1 bg-dark text-dark border">10</div>
+            <div class="col-1 bg-dark text-dark border">11</div>
+        </div>
+        <div class="row">
+            <div class="col-1 bg-dark text-dark border">1</div>
+            <div class="col-1 bg-dark text-dark border">2</div>
+            <div class="col-1 bg-dark text-dark border">3</div>
+            <div class="col-1 bg-white text-white">4</div>
+            <div class="col-1 bg-danger text-danger border">5</div>
+            <div class="col-1 bg-warning text-warning border">6</div>
+            <div class="col-1 bg-danger text-danger border">7</div>
+            <div class="col-1 bg-white text-white">8</div>
+            <div class="col-1 bg-dark text-dark border">9</div>
+            <div class="col-1 bg-dark text-dark border">10</div>
+            <div class="col-1 bg-dark text-dark border">11</div>
+        </div>
+        <div class="row">
+            <div class="col-1 bg-white text-white">1</div>
+            <div class="col-1 bg-dark text-dark border">2</div>
+            <div class="col-1 bg-dark text-dark border">3</div>
+            <div class="col-1 bg-dark text-dark border">4</div>
+            <div class="col-3 bg-white text-white">5-6-7</div>
+            <div class="col-1 bg-dark text-dark border">8</div>
+            <div class="col-1 bg-dark text-dark border">9</div>
+            <div class="col-1 bg-dark text-dark border">10</div>
+        </div>
+        <div class="row">
+            <div class="col-2 bg-white text-white">1-2</div>
+            <div class="col-1 bg-dark text-dark border">3</div>
+            <div class="col-1 bg-dark text-dark border">4</div>
+            <div class="col-1 bg-dark text-dark border">5</div>
+            <div class="col-1 bg-dark text-dark border">6</div>
+            <div class="col-1 bg-dark text-dark border">7</div>
+            <div class="col-1 bg-dark text-dark border">8</div>
+            <div class="col-1 bg-dark text-dark border">9</div>
+        </div>
+        <div class="row">
+            <div class="col-3 bg-white text-white">1-3</div>
+            <div class="col-1 bg-dark text-dark border">4</div>
+            <div class="col-1 bg-dark text-dark border">5</div>
+            <div class="col-1 bg-dark text-dark border">6</div>
+            <div class="col-1 bg-dark text-dark border">7</div>
+            <div class="col-1 bg-dark text-dark border">8</div>
+        </div>
+        <div class="row">
+            <div class="col-4 bg-white text-white">1-4</div>
+            <div class="col-1 bg-dark text-dark border">5</div>
+            <div class="col-1 bg-dark text-dark border">6</div>
+            <div class="col-1 bg-dark text-dark border">7</div>
+        </div>
+        <div class="row">
+            <div class="col-4 bg-white text-white">1-4</div>
+            <div class="col-1 bg-dark text-dark border">5</div>
+            <div class="col-1 bg-dark text-dark border">6</div>
+            <div class="col-1 bg-dark text-dark border">7</div>
+        </div>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
+</body>
+
+</html>
